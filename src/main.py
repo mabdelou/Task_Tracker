ed36c05664bc3fb05b09bb4ec08cefaf82a4d6f8
Alpha User <programming.abdu@gmail.com>
2024-08-24 18:52:29 +0100

    some small update
     this code may not be the best for large database, maybe i will upgrade it in future, to let it more faster using a good algorithem

--- src/main.py
+++ src/main.py
@@ -7,11 +7,11 @@ JSON_PATH = "./data/data.json"
 
 
 
-def ft_exit(exit_status:int, exit_msg:str):
+def ft_exit(exit_status:int, exit_msg:str) -> None:
     print(exit_msg)
     exit(exit_status)
 
-def ft_help(mood:str):
+def ft_help(mood:str) -> None:
     if mood == "add":
         print("for adding new  task use this syntax:\n")
         print("     EXECFILE add DISCRIPTION")
@@ -63,7 +63,7 @@ def get_next_id()-> tuple[int,int]:
 
 # create task
 
-def create_task(discreption:str) -> bool:
+def create_task(discreption:str) -> None:
     
     id:int = get_next_id()[0]
     new_task:dict = {"id":id,"discription":discreption,"status":"todo","createdAt":str(Date.now()),"updatedAt":str(Date.now())}
@@ -77,59 +77,63 @@ def create_task(discreption:str) -> bool:
             old_data["next-index"] = int(old_data["next-index"]) + 1
             with open(file=JSON_PATH,mode="w+",encoding="utf-8") as json_file:
                 json.dump(old_data,json_file,indent=4)
-            
-    
-    
-    return True
+            print(f"Task added successfully (ID: {id})")
 
 
 
 #update task
 
-def update_task(mood:str, value: str, index: int) -> bool:
+def update_task(mood:str, value: str, index: int) -> None:
     
-    id:int = get_next_id()[0]
-    if id == 1 or id < index:
-        ft_exit(5,"Index out of range")
+    id,lenght = get_next_id()
+    if id == 1 or lenght == 0:
+        ft_exit(5,"No data was saved")
+    elif id < index:
+        ft_exit(6,"Index out of range")
     elif value == "":
-        ft_exit(6,"Value should be not empty")
+        ft_exit(7,"Value should be not empty")
     elif index < 0:
-        ft_exit(7,"Negative numbers are note valid")
+        ft_exit(8,"Negative numbers are note valid")
     else: 
         with open(file=JSON_PATH,mode="r",encoding="utf-8") as create_json_file:
             old_data:dict = json.load(create_json_file)
-            if mood == "update":
-                old_data["tasks"][index]["discription"] = value
-                old_data["tasks"][index]["updatedAt"] = str(Date.now())
-            elif mood == "mark-in-progress" or mood == "mark-done":
-                old_data["tasks"][index]["status"] = value
-                old_data["tasks"][index]["updatedAt"] = str(Date.now())
-            with open(file=JSON_PATH,mode="w+",encoding="utf-8") as json_file:
-                json.dump(old_data,json_file,indent=4)
+            try:
+                for i,task in enumerate(old_data["tasks"]):
+                    if task["id"] == index:
+                        if mood == "update":
+                            old_data["tasks"][i]["discription"] = value
+                            old_data["tasks"][i]["updatedAt"] = str(Date.now())
+                        elif mood == "mark-in-progress" or mood == "mark-done":
+                            old_data["tasks"][i]["status"] = value
+                            old_data["tasks"][i]["updatedAt"] = str(Date.now())
+                with open(file=JSON_PATH,mode="w+",encoding="utf-8") as json_file:
+                    json.dump(old_data,json_file,indent=4)
+            except IndexError:
+                print("404 target not found")
 
-
-    return True
 
 
 #delete task
 
-def delete_task(index: int) -> bool:
+def delete_task(index: int) -> None:
     
-    id:int = get_next_id()[0]
-    if id == 1 or id < index:
-        ft_exit(9,"Index out of range")
+    id,lenght = get_next_id()
+    if id == 1  or lenght == 0:
+        ft_exit(10,"No data was saved")
+    elif id < index:
+        ft_exit(11,"Index out of range")
     elif index < 0:
-        ft_exit(10,"Negative numbers are note valid")
+        ft_exit(12,"Negative numbers are note valid")
         
     else: 
         with open(file=JSON_PATH,mode="r",encoding="utf-8") as create_json_file:
             old_data:dict = json.load(create_json_file)
-            old_data["tasks"] = [task for task in old_data["tasks"] if task["id"] != index]
-            with open(file=JSON_PATH,mode="w+",encoding="utf-8") as json_file:
-                json.dump(old_data,json_file,indent=4)
-
-
-    return True
+            try:
+                old_data["tasks"] = [task for task in old_data["tasks"] if task["id"] != index]
+                with open(file=JSON_PATH,mode="w+",encoding="utf-8") as json_file:
+                    json.dump(old_data,json_file,indent=4)
+            except IndexError:
+                print("404 target not found")
 
 
 
@@ -137,9 +141,9 @@ def delete_task(index: int) -> bool:
 
 def list_tasks(target:str|None=None):
     
-    id:int = get_next_id()[1]
-    if id == 0:
-        ft_exit(12,"No data was saved")
+    lenght:int = get_next_id()[1]
+    if lenght == 0:
+        ft_exit(14,"No data was saved")
     else: 
         with open(file=JSON_PATH,mode="r",encoding="utf-8") as create_json_file:
             old_data:dict = json.load(create_json_file)
@@ -148,7 +152,7 @@ def list_tasks(target:str|None=None):
             elif target == "done" or target == "todo" or target == "in-progress":
                 print([task for task in old_data["tasks"] if task["status"] == target])
             else:
-                ft_exit(13,"invalid information")
+                ft_exit(15,"invalid information")
 
     return True
 
@@ -161,31 +165,31 @@ try:
             ft_exit(1,"Wrong number of arguments")
     elif sys.argv[1] == "update":
         if len(sys.argv) == 4:
-            update_task(mood="update",value=sys.argv[3],index=int(sys.argv[2]) - 1)
+            update_task(mood="update",value=sys.argv[3],index=int(sys.argv[2]))
         else:
             ft_exit(2,"Wrong number of arguments")
     elif sys.argv[1] == "mark-in-progress":
         if len(sys.argv) == 3:
-            update_task(mood="mark-in-progress",value="in-progress",index=int(sys.argv[2]) - 1)
+            update_task(mood="mark-in-progress",value="in-progress",index=int(sys.argv[2]))
         else:
             ft_exit(3,"Wrong number of arguments")
     elif sys.argv[1] == "mark-done":
         if len(sys.argv) == 3:
-            update_task(mood="mark-done",value="done",index=int(sys.argv[2]) - 1)
+            update_task(mood="mark-done",value="done",index=int(sys.argv[2]))
         else:
             ft_exit(4,"Wrong number of arguments")
     elif sys.argv[1] == "delete":
         if len(sys.argv) == 3:
             delete_task(index=int(sys.argv[2]))
         else:
-            ft_exit(8,"Wrong number of arguments")
+            ft_exit(9,"Wrong number of arguments")
     elif sys.argv[1] == "list":
         if len(sys.argv) == 2:
             list_tasks()
         elif len(sys.argv) == 3:
             list_tasks(target=sys.argv[2])
         else:
-            ft_exit(11,"Wrong number of arguments")
+            ft_exit(13,"Wrong number of arguments")
     elif sys.argv[1] == "-h":
         if len(sys.argv) == 3:
             ft_help(mood=sys.argv[2])
@@ -195,4 +199,4 @@ try:
         raise ValueError
     
 except ValueError:
-    print("Wrong input please valid your input.\ncheck \"-h\" for more information about syntax")
+    print("Wrong input please valid your information.\ncheck \"-h\" for more information about syntax")
